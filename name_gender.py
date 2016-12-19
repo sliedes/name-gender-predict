7e7a90af8daf98459b1f1e0a59fcf2ea435a7790
Sami Liedes <sami.liedes@iki.fi>
2016-12-19 12:05:42 +0200

    predict

--- name_gender.py
+++ name_gender.py
@@ -13,6 +13,7 @@ import pandas as pd
 from keras.wrappers.scikit_learn import KerasRegressor
 from keras.callbacks import ModelCheckpoint, TensorBoard, CSVLogger
 from zlib import adler32
+from sklearn.metrics import mean_squared_error
 
 import re
 
@@ -30,6 +31,24 @@ MALE_IDX = 0
 FEMALE_IDX = 1
 
 MAX_EPOCHS = 200
+
+global GLO
+
+# FIXME global state
+class GLO:
+    chars = None
+    male_dic = None
+    female_dic = None
+    male = None
+    female = None
+    all_names = None
+    common_names = None
+    max_len = None
+    char_indices = None
+    indices_char = None
+    X = None
+    y = None
+    group_nums = None
 
 def load_names(fname):
     names = pd.read_csv(fname, sep=';')
@@ -54,104 +73,88 @@ def load_names(fname):
 def group_of_name(name):
     return adler32(name.encode('utf-8')) % NUM_SPLITS
 
-def main():
-    male_dic = load_names('male.csv')
-    female_dic = load_names('female.csv')
-    male = set(male_dic.keys())
-    female = set(female_dic.keys())
-    all_names = male | female
-    common_names = male & female
+# build the model: a single LSTM
+def build_model():
+    print('Build model...')
+    model = Sequential()
+    model.add(GRU(128, input_shape=(GLO.max_len, len(GLO.chars)), activation=act, dropout_W=.3, dropout_U=.3, return_sequences=True))
+    model.add(GRU(128, dropout_W=.3, dropout_U=.3))
+    model.add(Dense(2, activation='sigmoid'))
+
+    #optimizer = RMSprop(lr=0.01)
+    #optimizer = Adadelta()
+    #optimizer = Adam(lr=0.01)
+    optimizer = 'nadam'
+    model.compile(loss='binary_crossentropy', optimizer=optimizer)
+    return model
+
+def load_data():
+    GLO.male_dic = load_names('male.csv')
+    GLO.female_dic = load_names('female.csv')
+    GLO.male = set(GLO.male_dic.keys())
+    GLO.female = set(GLO.female_dic.keys())
+    GLO.all_names = GLO.male | GLO.female
+    GLO.common_names = GLO.male & GLO.female
 
     common_del = set()
     # Go through names both genders have; if one is vastly more common, choose it
-    for name in common_names:
-        m = male_dic[name]
-        f = female_dic[name]
+    for name in GLO.common_names:
+        m = GLO.male_dic[name]
+        f = GLO.female_dic[name]
         if m/f >= MORE_COMMON_THRESH:
             #print(name, 'is common, but {} males and {} females; considering male'.format(m, f))
-            del female_dic[name]
-            female.remove(name)
+            del GLO.female_dic[name]
+            GLO.female.remove(name)
         elif f/m >= MORE_COMMON_THRESH:
             #print(name, 'is common, but {} males and {} females; considering female'.format(m, f))
-            del male_dic[name]
-            male.remove(name)
+            del GLO.male_dic[name]
+            GLO.male.remove(name)
         else:
             continue
         common_del.add(name)
 
-    common_names -= common_del
+    GLO.common_names -= common_del
 
     print('{} male, {} female names, {} both'.format(
-        len(male), len(female), len(common_names)))
+        len(GLO.male), len(GLO.female), len(GLO.common_names)))
 
-    max_len = max([len(x) for x in all_names])
-    print('Longest name: {} chars'.format(max_len))
+    GLO.max_len = max([len(x) for x in GLO.all_names])
+    print('Longest name: {} chars'.format(GLO.max_len))
 
-    chars = ''.join(sorted(set(''.join(all_names))))
-    print('total chars:', len(chars))
-    print(chars)
+    GLO.chars = ''.join(sorted(set(''.join(GLO.all_names))))
+    print('total chars:', len(GLO.chars))
+    print(GLO.chars)
 
-    char_indices = dict((c, i) for i, c in enumerate(chars))
-    indices_char = dict((i, c) for i, c in enumerate(chars))
+    GLO.char_indices = dict((c, i) for i, c in enumerate(GLO.chars))
+    GLO.indices_char = dict((i, c) for i, c in enumerate(GLO.chars))
 
     print('Vectorization...')
-    X = np.zeros((len(all_names), max_len, len(chars)), dtype=np.bool)
-    y = np.zeros((len(all_names), 2), dtype=np.bool)
-    for i, name in enumerate(sorted(all_names)):
+    GLO.X = np.zeros((len(GLO.all_names), GLO.max_len, len(GLO.chars)), dtype=np.bool)
+    GLO.y = np.zeros((len(GLO.all_names), 2), dtype=np.bool)
+    for i, name in enumerate(sorted(GLO.all_names)):
         for j, char in enumerate(name):
-            X[i][j][char_indices[char]] = 1
-        if name in male:
-            y[i][MALE_IDX] = 1
-        if name in female:
-            y[i][FEMALE_IDX] = 1
+            GLO.X[i][j][GLO.char_indices[char]] = 1
+        if name in GLO.male:
+            GLO.y[i][MALE_IDX] = 1
+        if name in GLO.female:
+            GLO.y[i][FEMALE_IDX] = 1
 
     # split into NUM_SPLITS groups, deterministically based on the name
-    group_nums = np.array([group_of_name(x) for x in sorted(all_names)])
+    GLO.group_nums = np.array([group_of_name(x) for x in sorted(GLO.all_names)])
     print('Group sizes:')
     print('Male\tFemale\tCommon\tTotal')
     for i in range(NUM_SPLITS):
-        b = group_nums == i
+        b = GLO.group_nums == i
         print('{}\t{}\t{}\t{}'.format(
-            y[b, MALE_IDX].sum(), y[b, FEMALE_IDX].sum(),
-            ((y[b, MALE_IDX] == 1) & (y[b, FEMALE_IDX] == 1)).sum(),
-            len(y[b])))
+            GLO.y[b, MALE_IDX].sum(), GLO.y[b, FEMALE_IDX].sum(),
+            ((GLO.y[b, MALE_IDX] == 1) & (GLO.y[b, FEMALE_IDX] == 1)).sum(),
+            len(GLO.y[b])))
 
-    # build the model: a single LSTM
-    def build_model():
-        print('Build model...')
-        model = Sequential()
-        #act = lambda x: K.relu(x, max_value=1.0)
-        act = K.relu
-        # -- with validation_split=.2:
-        # 16, dropouts=.0: val_loss=.32
-        # 32, dropouts=.0: val_loss=.31
-        # 32, dropouts=.1: val_loss=.2780
-        # 32, dropouts=.5: val_loss=.35 (terminated early)
-        # 64, dropouts=.2: val_loss=.2655 (terminated early)
-        # 64, dropouts=.3: val_loss=.28
-        # 128, dropouts=.3: val_loss=.2645
-        # 128-bidi, dropouts=.5: val_loss=.2566 (terminated early?)
-        # 256, dropouts=.3: val_loss=.2510
-        # 256, dropouts=.4: val_loss=.2356
-        # **128x2, dropouts=.3: val_loss=.2325
-        # 128x2-bidi, dropouts=.4, val_loss=.2491 (terminated early?)
-        # 256x2-bidi, dropouts=.5, val_loss=.2517
-        #model.add(Bidirectional(GRU(128, input_shape=(max_len, len(chars)), dropout_W=.5, dropout_U=.5, return_sequences=True), input_shape=(max_len, len(chars))))
-        model.add(GRU(128, input_shape=(max_len, len(chars)), activation=act, dropout_W=.3, dropout_U=.3, return_sequences=True))
-        model.add(GRU(128, dropout_W=.3, dropout_U=.3))
-        #model.add(GRU(256, input_shape=(max_len, len(chars)), activation=act, dropout_W=.3, dropout_U=.3))
-        model.add(Dense(2, activation='sigmoid'))
-        #model.add(Activation('softmax'))
 
-        #optimizer = RMSprop(lr=0.01)
-        #optimizer = Adadelta()
-        #optimizer = Adam(lr=0.01)
-        optimizer = 'nadam'
-        model.compile(loss='binary_crossentropy', optimizer=optimizer)
-        return model
-
+def train():
+    load_data()
     for val_group in range(NUM_SPLITS):
-        validate = group_nums == val_group
+        validate = GLO.group_nums == val_group
         train = ~validate
         print('Training group {} with a split of {}+{} ({:.2f})'.format(
             val_group, train.sum(), validate.sum(), (train.sum()/(len(train)))))
@@ -161,8 +164,28 @@ def main():
             filepath="model_group" + str(val_group) + "_e{epoch:04d}-{val_loss:.4f}.h5",
             monitor='val_loss', verbose=1, save_best_only=True)
         logger = CSVLogger('group{}_train.csv'.format(val_group))
-        reg.fit(X[train], y[train], validation_data=(X[validate], y[validate]),
+        reg.fit(GLO.X[train], GLO.y[train], validation_data=(GLO.X[validate], GLO.y[validate]),
                 callbacks=[checkpointer, logger, TensorBoard(log_dir='tensorboard{}'.format(val_group))])
 
+def predict():
+    load_data()
+    all_names_arr = np.array(sorted(GLO.all_names), dtype=object)
+    data = []
+    for pred_group in range(NUM_SPLITS):
+        print(pred_group)
+        pred = GLO.group_nums == pred_group
+        pX = GLO.X[pred]
+        py = GLO.y[pred]
+        model = load_model('model_group{}.h5'.format(pred_group))
+        a = model.predict_on_batch(pX)
+        for name, real_class, pred_class in zip(all_names_arr[pred], py, a):
+            mse = mean_squared_error(real_class, pred_class)
+            data.append((str(name),) + tuple(real_class) + tuple(pred_class) + (mse,))
+    data = pd.DataFrame(data, columns=['name', 'real_male', 'real_female', 'pred_male', 'pred_female', 'MSE'])
+    data = data.sort_values(by='MSE')
+    data.to_csv('pred.csv', index=False)
+    print(data)
+
 if __name__ == '__main__':
-    main()
+    #train()
+    predict()
